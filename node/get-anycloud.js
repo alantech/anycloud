1568ac65a24af7d9243c861552eceed8641eef56
David Ellis <isv.damocles@gmail.com>
2021-02-23 20:13:56 -0800

    Windows doesn't have curl

--- node/get-anycloud.js
+++ node/get-anycloud.js
@@ -6,15 +6,19 @@ const path = require('path');
 const pjson = require('./package.json');
 
 const anycloudUrlBase = `https://github.com/alantech/anycloud/releases/download/v${pjson.version}/`;
+let request = '';
 let extract = '';
 let file = 'anycloud-ubuntu.tar.gz';
 if (process.platform === 'darwin') {
   file = 'anycloud-macos.tar.gz';
+  request = `curl -OL ${anycloudUrlBase}${file}`;
   extract = `tar -xzf ${file}`;
 } else if (process.platform === 'win32') {
   file = 'anycloud-windows.zip';
+  request = `Invoke-WebRequest -OutFile anycloud-windows.zip -Uri ${anycloudUrlBase}${file}`;
   extract = 'Expand-Archive -Path anycloud-windows.zip -DestinationPath .';
 } else {
+  request = `curl -OL ${anycloudUrlBase}${file}`;
   extract = `tar -xzf ${file}`;
 }
 
@@ -25,7 +29,7 @@ exec('mkdir bin', (error, stdout, stderr) => {
     process.exit(1);
   }
   const cwd = path.join(process.cwd(), 'bin')
-  exec(`curl -OL ${anycloudUrlBase}${file}`, { cwd, }, (error, stdout, stderr) => {
+  exec(request, { cwd, }, (error, stdout, stderr) => {
     if (error) {
       console.log(stdout);
       console.error(stderr);
