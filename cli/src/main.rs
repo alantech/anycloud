5b7542b3b5550f81280282441aafa485abc88394
Alejandro Guillen <aguillenv93@gmail.com>
2021-03-04 17:38:21 +0100

    Try with wrong path

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -48,8 +48,7 @@ fn get_app_tar_gz_b64() -> String {
 #[tokio::main]
 pub async fn main() {
   let pwd = std::env::var("PWD").unwrap();
-  println!("{}", pwd);
-  let anycloud_agz = base64::encode(include_bytes!(format!("{}/alan/anycloud.agz", pwd.to_string())));
+  let anycloud_agz = base64::encode(include_bytes!("/alan/anycloud.agz"));
   let app = App::new(crate_name!())
     .version(crate_version!())
     .about("AnyCloud is a Lambda alternative that works with multiple cloud provider.")
