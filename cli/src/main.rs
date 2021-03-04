d7a0741b394bf9dad69a34666c09f9b4bf725f93
Alejandro Guillen <aguillenv93@gmail.com>
2021-03-04 16:55:35 +0100

    cast to str

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -49,7 +49,7 @@ fn get_app_tar_gz_b64() -> String {
 pub async fn main() {
   let pwd = std::env::var("PWD").unwrap();
   println!("{}", pwd);
-  let anycloud_agz = base64::encode(include_bytes!(format!("{}/alan/anycloud.agz", pwd as String)));
+  let anycloud_agz = base64::encode(include_bytes!(format!("{}/alan/anycloud.agz", pwd.to_string())));
   let app = App::new(crate_name!())
     .version(crate_version!())
     .about("AnyCloud is a Lambda alternative that works with multiple cloud provider.")
