463925e92a56498c1074dfd159614f6037b4b434
Alejandro Guillen <aguillenv93@gmail.com>
2021-03-05 18:10:17 +0100

    Add revision to recompile

--- Formula/anycloud.rb
+++ Formula/anycloud.rb
@@ -4,6 +4,7 @@ class Anycloud < Formula
     url "https://github.com/alantech/anycloud/archive/v0.1.2.tar.gz"
     sha256 "3ca6c849aace024c75e5429db95b37e1a5592902557439996508b469cc706370"
     license "Apache-2.0"
+    revision 1
   
     depends_on "rust" => :build
   
