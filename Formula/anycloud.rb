50a4311611a83382c0579ad357d8adc5c86cc6aa
Alejandro Guillen <aguillenv93@gmail.com>
2021-03-05 18:12:44 +0100

    Update alan checksum

--- Formula/anycloud.rb
+++ Formula/anycloud.rb
@@ -4,13 +4,13 @@ class Anycloud < Formula
     url "https://github.com/alantech/anycloud/archive/v0.1.2.tar.gz"
     sha256 "3ca6c849aace024c75e5429db95b37e1a5592902557439996508b469cc706370"
     license "Apache-2.0"
-    revision 1
+    revision 2
   
     depends_on "rust" => :build
   
     resource "alan" do
       url "https://github.com/alantech/alan/releases/download/v0.1.30/alan-macos.tar.gz"
-      sha256 "456a801d9f4c0b4e2e4e926ee2f98ea0afc12f93b4e860306cbb0bb109b5ae9d"
+      sha256 "dd2ac8f9f056c1ea906ed26300d9168da29c3ee0511ea43a96040fcc5f4c7ea6"
     end
   
     def install
