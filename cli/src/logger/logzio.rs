11c9c7298c184b1544c341fb27820956ea7b04fa
Alejandro Guillen <aguillenv93@gmail.com>
2021-03-23 18:00:34 +0100

    Refactor to handle token per env

--- cli/src/logger/logzio.rs
+++ cli/src/logger/logzio.rs
@@ -14,16 +14,29 @@ impl log::Log for LogzIO {
   }
 
   fn log(&self, record: &Record) {
+    let env = std::env::var("ALAN_TECH_ENV").unwrap_or("production".to_string());
+    let token = match env.as_str() {
+      "local" => "ZERXpCvywsOBtNOXrqIzfpLiOnEXKXhb",
+      "staging" => "ZERXpCvywsOBtNOXrqIzfpLiOnEXKXhb",
+      "production" => "ZERXpCvywsOBtNOXrqIzfpLiOnEXKXhb",
+      _ => "",
+    };
     let local_time = Local::now();
     let utc_time = DateTime::<Utc>::from_utc(local_time.naive_utc(), Utc);
-    let url = "https://listener.logz.io:8071/?token=ZERXpCvywsOBtNOXrqIzfpLiOnEXKXhb&type=anycloud";
+    let url = format!(
+      "https://listener.logz.io:8071/?token={}&type=anycloud",
+      token
+    );
     if self.enabled(record.metadata()) {
       let client = Client::builder().build::<_, Body>(hyper_tls::HttpsConnector::new());
-      let req = Request::post(url)
-        .body(json!({
+      let req = Request::post(url).body(
+        json!({
           "level": record.level(),
           "message": format!("{} | {} | {}", utc_time, record.level(), record.args()),
-        }).to_string().into());
+        })
+        .to_string()
+        .into(),
+      );
       let req = match req {
         Ok(req) => req,
         Err(e) => {
--- cli/src/logger/sematext.rs
+++ cli/src/logger/sematext.rs
@@ -19,12 +19,18 @@ impl log::Log for Sematext {
   }
 
   fn log(&self, record: &Record) {
+    let env = std::env::var("ALAN_TECH_ENV").unwrap_or("production".to_string());
+    let token = match env.as_str() {
+      "local" => "f3c3fe7c-9689-470c-98c6-bc60e9b9649d",
+      "staging" => "f3c3fe7c-9689-470c-98c6-bc60e9b9649d",
+      "production" => "f3c3fe7c-9689-470c-98c6-bc60e9b9649d",
+      _ => "",
+    };
     let local_time = Local::now();
     let utc_time = DateTime::<Utc>::from_utc(local_time.naive_utc(), Utc);
     if self.enabled(record.metadata()) {
       let future = ES_CLIENT
-        // TODO: make this key dynamic based on env
-        .index(IndexParts::Index("f3c3fe7c-9689-470c-98c6-bc60e9b9649d"))
+        .index(IndexParts::Index(token))
         .body(json!({
           "level": record.level(),
           "message": format!("{} | {} | {}", utc_time, record.level(), record.args()),
