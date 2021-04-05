9a167f1ace9fa5b1e28cf99fe4d5dc05e5949a85
Alejandro Guillen <aguillenv93@gmail.com>
2021-04-05 13:47:06 +0200

    Add daemon codes

--- cli/src/logger/mod.rs
+++ cli/src/logger/mod.rs
@@ -14,6 +14,14 @@ pub enum ErrorKind {
   InvalidDefaultCredentialAlias = 111,
   InvalidCredentialAlias = 112,
   AuthFailed = 113,
+  NoDnsVms = 114,
+  PostStats = 115,
+  NoClusterSecret = 116,
+  NoDns = 117,
+  NoPrivateIp = 118,
+  NoDnsPrivateIp = 119,
+  ScaleFailed = 120,
+  PostFailed = 121,
 }
 
 #[macro_export]
